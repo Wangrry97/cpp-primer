144165e5c53760eff3d4a73727bbb31b52d3ea2d
Wangrry97 <wangrry@hotmail.com>
2023-03-08 02:02:30 +0800

    调试失败

--- Chapter_2/17.cpp
+++ Chapter_2/17.cpp
@@ -3,10 +3,11 @@
  * @Author: Wang Renruoyu
  * @Date: 2023-02-28 15:44:53
  * @LastEditTime: 2023-02-28 15:46:14
- * @Descripttion: 
+ * @Descripttion:
  */
-#include<iostream>
-int main(){
+#include <iostream>
+int main()
+{
     int i, &ri = i;
     i = 5;
     ri = 10;

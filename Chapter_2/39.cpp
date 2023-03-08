c4c7a6cc48db9e9e0a80eec553cf0264a9e3cfc6
wrry <wangrry@hotmail.com>
2023-03-08 22:03:46 +0800

    2.6.1节练习

---
+++ Chapter_2/39.cpp
@@ -0,0 +1,7 @@
+struct Foo
+{
+    /* data */
+} int main()
+{
+    return 0;
+}
---
+++ Chapter_2/40.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Salas_data
+{
+    string bookName;
+    string bookNo;
+    string ISBN;
+    int units_sold = 0;
+    double originalPrice = 0.0;
+    double soldPrice = 0.0;
+    double discount = 0.0;
+};
+int main()
+{
+    return 0;
+}

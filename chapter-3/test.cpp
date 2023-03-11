eda68e46338e8dad26633abb8c100c2899b015e5
wrry <wangrry@hotmail.com>
2023-03-11 18:24:47 +0800

    新建了测试用的程序，想要查看未初始化的string变量的值

---
+++ chapter-3/test.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: wrry
+ * @email: wangrry@hotmail.com
+ * @Date: 2023-03-11 18:19:26
+ * @LastEditors: wrry wangrry@hotmail.com
+ * @LastEditTime: 2023-03-11 18:20:07
+ * @FilePath: /cpp-primer/chapter-3/test.cpp
+ * @Description:
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s1;
+    return 0;
+}
